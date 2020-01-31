2d20f2f7d57fb92ad677faa5f5ef72b060284162
Renju Liu <i.am.renju.liu@gmail.com>
2020-01-30 23:04:41 -0800

    format encryption algorithms finished.

--- optee_os/core/include/crypto/secdeep_fpe.h
+++ optee_os/core/include/crypto/secdeep_fpe.h
@@ -1,44 +1,26 @@
-//Format preserving encryption header file.
-# define ceil2(x, bit) ( ((x) >> (bit)) + ( ((x) & ((1 << (bit)) - 1)) > 0 ) )
-# define floor2(x, bit) ( (x) >> (bit) )
-void pow_uv(BIGNUM *pow_u, BIGNUM *pow_v, unsigned int x, int u, int v, BN_CTX *ctx);
+// Format encryption definition here.
+#define ENCRYPTION 1
+#define DECRYPTION 0
 
-#ifndef HEADER_FPE_H
-# define HEADER_FPE_H
+#define SECDEEP_KEY_LEN 16
 
-# include <openssl/aes.h>
+// #include <crypto/crypto.h>
+#include <kernel/panic.h>
+#include <tee_api_defines.h>
+#include <tee_api_types.h>
+#include <assert.h>
+#include <crypto/crypto.h>
+#include <crypto/crypto_impl.h>
+#include <stdlib.h>
+#include <tee_api_types.h>
+#include <tomcrypt.h>
+#include <util.h>
 
-# ifdef __cplusplus
-extern "C" {
-# endif
+uint8_t SECDEEP_IV[]  = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+  0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
+uint8_t SECDEEP_KEY[] = {0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6,
+  0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c}; //128 bits
 
-# define FPE_ENCRYPT 1
-# define FPE_DECRYPT 0
-
-# define FF1_ROUNDS 10
-# define FF3_ROUNDS 8
-# define FF3_TWEAK_SIZE 8
-
-struct fpe_key_st {
-    unsigned int radix;
-    unsigned int tweaklen;
-    unsigned char *tweak;
-    AES_KEY aes_enc_ctx;
-};
-
-typedef struct fpe_key_st FPE_KEY;
-
-/*** FF1 ***/
-int FPE_set_ff1_key(const unsigned char *userKey, const int bits, const unsigned char *tweak, const unsigned int tweaklen, const int radix, FPE_KEY *key);
-
-void FPE_unset_ff1_key(FPE_KEY *key);
-
-void FPE_ff1_encrypt(unsigned int *in, unsigned int *out, unsigned int inlen, FPE_KEY *key, const int enc);
-
-void FPE_ff1_encrypt_char(unsigned char *in, unsigned char *out, unsigned int inlen, FPE_KEY *key, const int enc);
-
-# ifdef __cplusplus
-}
-# endif
-
-#endif
+int init_FPE_state(symmetric_CTR *ctr);
+void FPE_encrypt(unsigned char *plaintext, unsigned char *ciphered, unsigned int inlen);
+void FPE_decrypt(unsigned char *plaintext, unsigned char *ciphered, unsigned int inlen);
