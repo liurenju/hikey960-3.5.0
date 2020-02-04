c9e0da582806028ee08dc4ba633635adddd5fa31
Renju Liu <i.am.renju.liu@gmail.com>
2020-02-03 20:34:26 -0800

    hash table enabled. Needs to be tested tomorrow.

--- optee_os/core/include/kernel/secdeep_table.h
+++ optee_os/core/include/kernel/secdeep_table.h
@@ -1,1 +1,30 @@
 // The secdeep table related implementations.
+#include <stdlib.h>
+#include <trace.h>
+#include <assert.h>
+#include <compiler.h>
+#include <keep.h>
+#include <kernel/asan.h>
+#include <kernel/panic.h>
+#include <string.h>
+#include <types_ext.h>
+#include <util.h>
+
+typedef struct val {
+  char *value;
+  char length;
+} VAL;
+
+#define HASH1_SIZE  100000
+#define HASH2_SIZE  100000
+#define HASH_MAX    100000
+
+int* hashL1[HASH1_SIZE];
+char initialized[HASH1_SIZE];
+char entry = 0;
+
+void secdeep_hash_init(void);
+int add_entry(int key);
+int hash_get_value(int key, int* value);
+int hash_add_pair(int key, int value);
+int secdeep_hash_delete(void);
--- optee_os/core/kernel/secdeep_table.c
+++ optee_os/core/kernel/secdeep_table.c
@@ -1,1 +1,68 @@
 // The implementation of secdeep table.
+#include <kernel/secdeep_table.h>
+
+void secdeep_hash_init(void) {
+  for(int i = 0; i < HASH1_SIZE; i++) {
+    initialized[i] = 0;
+  }
+}
+
+// Expand or add the entry to hash table
+int add_entry(int key) {
+  if(!entry) {
+    secdeep_hash_init();
+    entry = 1;
+  }
+
+  int index = key / HASH1_SIZE;
+  if (index >= HASH_MAX) {
+    DMSG("Max hash value exceeded.");
+    return 1;
+  }
+
+  if(initialized[index]) {
+    DMSG("This entry has been initialized.");
+    return 1;
+  }
+  initialized[index] = 1;
+  hashL1[index] = (int *)malloc(sizeof(int) * HASH2_SIZE);
+  return 0;
+}
+
+int hash_get_value(int key, int* value) {
+  int index = key / HASH1_SIZE;
+  if (index >= HASH_MAX) {
+    DMSG("Cannot get the hash value. Too large!");
+    return 1;
+  }
+
+  if (!initialized[index]) {
+    DMSG("Cannot get the hash value. Too large!");
+    return 1;
+  }
+
+  int entry_l2 = key % HASH2_SIZE;
+  *value = hashL1[index][entry_l2];
+  return 0;
+}
+
+int hash_add_pair(int key, int value) {
+  int index = key / HASH1_SIZE;
+  if (index >= HASH_MAX) {
+    DMSG("Max hash value exceeded.");
+    return 1;
+  }
+
+  add_entry(key);
+  int entry_l2 = key % HASH2_SIZE;
+  hashL1[index][entry_l2] = value;
+  return 0;
+}
+
+int secdeep_hash_delete(void) {
+  for(int i = 0; i < HASH1_SIZE; i++) {
+    initialized[i] = 0;
+  }
+  entry = 0;
+  return 0;
+}
